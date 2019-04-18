509edfd231cb67ca4be62bfe58a5bcadec0125fe
AhmadNabiAhmadi <49725377+AhmadNabiAhmadi@users.noreply.github.com>
2019-04-18 14:59:14 +0530

    Add files via upload
    (Python is Easy) course first completed  Homework #1

---
+++ main.py
@@ -0,0 +1,27 @@
+
+'''
+
+This is Python program for
+Homework #1
+
+'''
+
+Song = "Tum Hi Ho"
+Released_Date = 2017
+Recorded_year = 2013
+Genre = "Romantic"
+Song_length = 443
+Label = "T-Series"
+Song_Writers = "Mithon"
+Singer = "Arijit Sing"
+
+
+
+print(Song)
+print(Released_Date)
+print(Recorded_year)
+print(Genre)
+print(Song_length)
+print(Label)
+print(Song_Writers)
+print(Singer)
